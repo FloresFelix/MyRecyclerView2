edac3a5556a1859ac8e85490409c08116f7c3711
FloresFelix <52514937+FloresFelix@users.noreply.github.com>
2020-01-10 19:29:30 -0300

    primer commit 10/01/2020 v4

---
+++ app/src/main/java/com/example/myrecyclerview2/Conector.java
@@ -0,0 +1,14 @@
+package com.example.myrecyclerview2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Conector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conector);
+    }
+}
