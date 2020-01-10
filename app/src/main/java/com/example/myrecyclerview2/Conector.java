c5fe35f0f7bf11ad6bd4eb897daa2ae16c1b11d5
FloresFelix <52514937+FloresFelix@users.noreply.github.com>
2020-01-10 19:33:19 -0300

    primer commit 10/01/2020 v4

--- app/src/main/java/com/example/myrecyclerview2/Conector.java
+++ app/src/main/java/com/example/myrecyclerview2/Conector.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Conector extends AppCompatActivity {
+    float tonio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
