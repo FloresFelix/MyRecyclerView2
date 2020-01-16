898d70bc66c0fafefffa60c4d740965f4c1c53d1
Pablo <multimediaixd@gmail.com>
2020-01-16 14:16:08 -0300

    subiendo desde la pc2 de ixd

--- app/src/main/java/com/example/myrecyclerview2/Conector.java
+++ app/src/main/java/com/example/myrecyclerview2/Conector.java
@@ -3,10 +3,17 @@ package com.example.myrecyclerview2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Conector extends AppCompatActivity {
     float tonio;
     int chiky;
+    TextView textView;
+    ImageView imageView;
+    //subido desde la pc de infinito por descubrir
     //subido deade la PC de Escritorio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
